c9eaec18214017d160a438705263b3bfd630c96e
Jalen Battle <jalenbattle78@gmail.com>
2024-11-25 15:02:14 -0500

    using classes and stuff

--- school.js
+++ school.js
@@ -23,32 +23,34 @@
 // What do teachers do?
 // - Teach
 
-class Student {
+// Base Class / Parent Class, we inherit from "Person"
+class Person{
     constructor(name, skills) {
         this.name = name;
         this.skills = skills;
-    }
+    };
+};
+
+class Student extends Person { // Student is_a Person inherits the constructor
+
     learn(skill){
         this.skills.push(skill);
-    }
-}
-class Teacher {
-    constructor(name, skills) {
-        this.name = name;
-        this.skills = skills;
-    }
+    };
+};
+class Teacher extends Person{ // Student is_a Person inherits the constructor
+
     teach(studenet, skill_index){
         studenet.learn(this.skills[skill_index])
-    }
-}
+    };
+};
 class School {
     // Compostion - has_a relationships netween Objects
     // A school is Composed of students and teachers
     constructor(teachers, students) {
         this.teachers = teachers; // School has teachers
         this.students = students; // School has students
-    }
-}
+    };
+};
 // Tests
 let s = new School([
     new Teacher("Bob",["code", "draw"]),
@@ -62,7 +64,7 @@ let alice = s.students[1];
 let bob = s.teachers[0];
 bob.teach(alice, 0);
 console.log(alice.skills[1]==="code")
-console.log(alice);
-console.log(bob);
+console.log(alice.skills);
+console.log(bob.skills);
 
 
